78184ad59535165d76e31c1840e19c1fc4f2af9d
Seohyun Yoon <sarahseohyun@gmail.com>
2022-08-11 20:12:22 +0900

    Time: 61 ms (94.08%), Space: 42.1 MB (52.98%) - LeetHub

---
+++ 876-middle-of-the-linked-list/876-middle-of-the-linked-list.js
@@ -0,0 +1,20 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+var middleNode = function(head) {
+    let slow = head;
+    while (slow && slow.next) {
+        head = head.next;
+        slow = slow.next.next;
+    }
+    
+    return head;
+};
