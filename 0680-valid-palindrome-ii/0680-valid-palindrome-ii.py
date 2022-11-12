182c354ec163e64495c847342fd769e73dc66c7a
Seohyun Yoon <sarahseohyun@gmail.com>
2022-11-12 13:45:38 +0900

    Time: 262 ms (60.67%), Space: 14.4 MB (89.36%) - LeetHub

---
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.py
@@ -0,0 +1,11 @@
+class Solution:
+    def validPalindrome(self, s: str) -> bool:
+        if s == s[::-1]:
+            return True
+        
+        i = 0
+        while i<=len(s)/2 and s[i]==s[-(i+1)]:
+            i+=1
+
+        s=s[i:len(s)-i]
+        return s[1:]==s[1:][::-1] or s[:-1]==s[:-1][::-1]
