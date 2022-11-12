9d9e9804c0b66fb7d38c210fcecebd1f80663cf4
Seohyun Yoon <sarahseohyun@gmail.com>
2022-11-12 14:50:05 +0900

    Time: 2863 ms (22.76%), Space: 36.1 MB (82.62%) - LeetHub

---
+++ 2391-minimum-amount-of-time-to-collect-garbage/2391-minimum-amount-of-time-to-collect-garbage.py
@@ -0,0 +1,31 @@
+class Solution:
+    def garbageCollection(self, garbage: List[str], travel: List[int]) -> int:
+        metal = 0
+        paper = 0
+        glass = 0
+
+        m = 0
+        p = 0
+        g = 0
+        
+        for idx, x in enumerate(garbage):
+            if 'M' in x:
+                metal = idx
+                m += x.count('M')
+            if 'G' in x:
+                glass = idx
+                g += x.count('G')
+            if 'P' in x:
+                paper = idx
+                p += x.count('P')
+
+        if metal:
+            for i in range(0, metal):
+                m += travel[i]
+        if glass:
+            for i in range(0, glass):
+                g += travel[i]
+        if paper:
+            for i in range(0, paper):
+                p += travel[i]
+        return m+g+p
