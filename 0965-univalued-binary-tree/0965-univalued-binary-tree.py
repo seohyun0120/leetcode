b533e1b88fd3899550a48120888e3f84eb9702ca
Seohyun Yoon <sarahseohyun@gmail.com>
2022-11-12 16:47:49 +0900

    Time: 59 ms (49.89%), Space: 13.8 MB (64.36%) - LeetHub

---
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.py
@@ -0,0 +1,19 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, val=0, left=None, right=None):
+#         self.val = val
+#         self.left = left
+#         self.right = right
+class Solution:
+    def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
+        res = []
+
+        def dfs(root):
+            if not root:
+                return
+            dfs(root.left)
+            res.append(root.val)
+            dfs(root.right)
+        
+        dfs(root)
+        return len(set(res)) == 1
