d02c0f33a9574ab80c5d8364f551bb8e13d09f7c
Seohyun Yoon <sarahseohyun@gmail.com>
2023-01-02 22:42:18 +0900

    Create 205-Isomorphic-Strings.py
    https://leetcode.com/problems/isomorphic-strings/submissions/869720280/

---
+++ 205-Isomorphic-Strings.py
@@ -0,0 +1,8 @@
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        d1, d2 = dict(), dict()
+        for v, w in zip(s,t):
+            if (v in d1 and d1[v] != w) or (w in d2 and d2[w] != v):
+                return False
+            d1[v], d2[w] = w, v
+        return True
